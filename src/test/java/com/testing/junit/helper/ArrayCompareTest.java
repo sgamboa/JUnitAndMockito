c8640de42e0c8b7a288208b9bf703f4b3515878f
Suh <gamboa.arias.s@gmail.com>
2019-02-20 18:31:26 -0300

    performance test

--- src/test/java/com/testing/junit/helper/ArrayCompareTest.java
+++ src/test/java/com/testing/junit/helper/ArrayCompareTest.java
@@ -19,4 +19,20 @@ public class ArrayCompareTest {
 		// assetEquals in Arrays compare if are the same arrays, not the content
 	}
 
+	@Test(expected = NullPointerException.class)
+	public void testArraySort_NullArray() {
+		int[] numbers = null;
+		Arrays.sort(numbers);
+	}
+
+	// depends on the server where this is running
+	@Test(timeout = 100)
+	public void testSort_Performance() {
+		int array[] = { 12, 23, 4 };
+		for (int i = 1; i <= 1000000; i++) {
+			array[0] = i;
+			Arrays.sort(array);
+		}
+	}
+
 }
