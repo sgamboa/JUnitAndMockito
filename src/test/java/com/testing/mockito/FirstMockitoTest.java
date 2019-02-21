7fc7a5acb583c2e1432e4ce6afed5e6b737b8511
Suh <gamboa.arias.s@gmail.com>
2019-02-21 17:12:10 -0300

    Setting SUT

---
+++ src/test/java/com/testing/mockito/FirstMockitoTest.java
@@ -0,0 +1,14 @@
+package com.testing.mockito;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FirstMockitoTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
