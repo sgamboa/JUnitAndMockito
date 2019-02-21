aa63dcec3330028bb58282be77bbd73f86159247
Suh <gamboa.arias.s@gmail.com>
2019-02-21 18:42:37 -0300

    Mockito example

---
+++ src/test/java/com/testing/business/TodoBusinessImplMockTest.java
@@ -0,0 +1,39 @@
+package com.testing.business;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.testing.data.api.TodoService;
+
+public class TodoBusinessImplMockTest {
+
+	@Test
+	public void testRetrieveTodosRelatedToSpring_usingAMock() {
+		TodoService todoServiceMock = mock(TodoService.class);
+		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn to Dance");
+		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
+
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
+
+		assertEquals(2, filteredTodos.size());
+	}
+
+	@Test
+	public void testRetrieveTodosRelatedToSpring_withEmptyList() {
+		TodoService todoServiceMock = mock(TodoService.class);
+		List<String> todos = Arrays.asList();
+		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
+
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
+
+		assertEquals(0, filteredTodos.size());
+	}
+}
